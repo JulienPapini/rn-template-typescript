06e5f7362ed727779f372c03a2c5c8c653d02697
Julien Papini <julien.papini@gmail.com>
2020-11-27 23:16:04 +0100

    rn-0.63-custom: Installing Eslint prettier configuration

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'airbnb-typescript', 'airbnb/hooks'],
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
